3922f471b28d95b03ec8e59181728b4f65c1e46f
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-10-03 15:29:35 +0300

    Updated: firebase Setup and Config

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAmag7ZLkEltQLMF19tsfFyy9pWd3n7uB4",
+    authDomain: "react-notes-d66f6.firebaseapp.com",
+    projectId: "react-notes-d66f6",
+    storageBucket: "react-notes-d66f6.appspot.com",
+    messagingSenderId: "584316601300",
+    appId: "1:584316601300:web:f8d77e0f48eb2579a04c41"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const notesCollection = collection(db, "notes");
