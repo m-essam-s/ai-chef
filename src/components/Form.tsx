7a7d34b79051b5f29d9a2c562c4e9d3efa430584
m-essam-s <sir.m.essam.g@gmail.com>
2025-02-06 18:35:17 +0200

    feat: Update form with action

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,21 @@
-const Form = () => {
+const Form = (
+    { setItem }: {
+        setItem: React.Dispatch<React.SetStateAction<string[]>>
+    }) => {
+    const addIngredient = (formData: FormData) => {
+        const ingredient = formData.get("ingredient") as string;
+        setItem((prevItem) => [...prevItem, ingredient]);
+    };
     return (
         <form
+            action={addIngredient}
             className="flex justify-center items-center gap-[12px] h-[38px]"
         >
             <input
                 type="text"
                 placeholder="Add ingredient"
                 aria-label="Add ingredient"
+                name="ingredient"
                 className="rounded-[6px] border-[1px] border-[#D1D5DB] px-[9px] py-[6px] min-w-[150px] max-w-[400px] shadow-[0px_1px_2px_0px_rgba(0,0,0,0.05)] flex-grow"
             />
             <button
